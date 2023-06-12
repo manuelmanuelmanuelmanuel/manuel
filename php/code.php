b1071af8aa8cc717ca8ba82667c573e7181843b8
manuelmanuelmanuelmanuel <136299397+manuelmanuelmanuelmanuel@users.noreply.github.com>
2023-06-12 03:21:32 -0300

    Add files via upload

---
+++ php/code.php
@@ -0,0 +1,42 @@
+<?php
+$to = "dataled@utdt.edu";
+$subject = "Contact from the website";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$name = $_POST['userName'];
+$email = $_POST['userEmail'];
+$comments = $_POST['userComments'];
+$age = $_POST['userAge'];
+
+
+$message = "
+<html>
+<head>
+<title>Contact Form</title>
+</head>
+<body>
+<h1>Contact information from the website</h1>
+<p>User name: $name </p> 
+<p>Email: $email </p> 
+<p>Comments: $comments </p> 
+<p>Age from the User: $age </p> 
+
+</body>
+</html>";
+
+ echo '<h1>Thanks</h1>
+
+ <script>
+setTimeout( function() { window.location.href = "https://sabrinaejemplo.000webhostapp.com/"; }, 10000 );
+ </script>
+ 
+ 
+ ';
+
+
+
+mail($to, $subject, $message, $headers);
+
+
+
+?>
